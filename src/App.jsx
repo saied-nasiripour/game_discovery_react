47cf18d5c6b3c10e216fae7cdda79732188db97f
saied-nasiripour <saied.nasiripour@gmail.com>
2024-04-12 17:14:07 +0330

    Implement dark theme styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./Pages/Home.jsx";
+import Header from "./Components/Header.jsx";
+import {ThemeContext} from "./Context/ThemeContext.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("dark");
+
 
   return (
-      <>
-          <div>
-              <a href="https://vitejs.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo"/>
-              </a>
-              <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo"/>
-              </a>
+      <ThemeContext.Provider value={{theme, setTheme}}>
+          <div className={`${theme} ${theme == 'dark' ? 'bg-[#121212]' : null} w-full h-[100vh]`}>
+              <div className="bg-white dark:bg-black">
+                  <div className="max-w-7xl mx-auto">
+                      <Header/>
+                      <Home/>
+                  </div>
+              </div>
           </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-              </button>
-              <p>
-                  Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
-          </div>
-          <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-          </p>
-      </>
+      </ThemeContext.Provider>
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,48 @@
+import {useContext, useEffect, useState} from "react";
+import logo from './../assets/Images/logo.png';
+import {HiOutlineMagnifyingGlass, HiMoon, HiSun} from "react-icons/hi2"
+import {ThemeContext} from "../Context/ThemeContext.jsx";
+
+function Header() {
+    const [toggle, setToggle] = useState(false);
+    const {theme, setTheme} = useContext(ThemeContext);
+
+    useEffect(
+        ()=>{
+            console.log("theme: ", theme);
+        }
+    );
+
+    return (
+        <div className="flex items-center justify-between p-3 w-full">
+            {/*First column for the image*/}
+            <div className="flex-shrink-0">
+                <img className="h-15 w-15" src={logo} alt="logo"/> {/* <!-- 60x60px image -->*/}
+            </div>
+            {/*Second column for the search box*/}
+            <div className='flex flex-grow bg-slate-200 p-2 mx-4 rounded-full items-center'>
+                <div className="items-center justify-center p-2">
+                    <HiOutlineMagnifyingGlass/>
+                </div>
+                <input type='text' placeholder="Search Games" className='w-full px-2 bg-transparent outline-none text-black'/>
+            </div>
+            {/*Third column for the theme switcher*/}
+            <div className='flex-shrink-0'>
+                {
+                    theme == ""
+                    ? <HiMoon
+                            className="w-9 h-9 bg-slate-200 text-black p-1 rounded-full cursor-pointer"
+                            /*onClick={() => setToggle(false)}*/
+                            onClick={() => setTheme("dark")}
+                        />
+                    : <HiSun
+                            className="w-9 h-9 bg-slate-200 text-black p-1 rounded-full cursor-pointer"
+                            /*onClick={() => setToggle(true)}*/
+                            onClick={() => setTheme("")}
+                        />
+                }
+            </div>
+        </div>
+    )
+}
+export default Header;
